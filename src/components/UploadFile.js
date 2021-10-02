3d16d7cd60d452305c4a1f509a02b3734ca7e27e
ahmed <ahmedlearn01@gmail.com>
2021-10-02 16:24:48 -0700

    initial commit

---
+++ src/components/UploadFile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const UploadFile = () => {
+    return (
+        <div>
+            <h1>Hi I'm the warrior, the programmer, the king, the genious him self</h1>
+        </div>
+    )
+}
+
+export default UploadFile;
