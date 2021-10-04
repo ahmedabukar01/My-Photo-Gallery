e4b2a1be8e526f5ffc2c2f237216933ab5d55251
ahmed <ahmedlearn01@gmail.com>
2021-10-04 06:56:52 -0700

    initial commit

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -1,13 +1,32 @@
 import React from 'react'
+import {useState} from 'react'
 
 export const UploadFile = () => {
+    const [file,setFile] = useState(null);
+    const [error,setError] = useState(null);
+    const onChange = (e)=>{
+        const type = ['image/png','image/jpeg'];
+        const selected = e.target.files[0];
+        
+        if(selected && type.includes(selected.type)){
+            setFile(selected);
+            setError(null)
+            
+        } else{
+            setError('please Selected valid image such as (png or jpeg');
+            setFile(null);
+        }
+        
+        
+    }
     return (
         <form className="upload-file container">
             <div className="fields">
-                <input type="file" id="file" />
-                <label for="file">
+                <input type="file" id="file" onChange={onChange}/>
+                <label htmlFor="file">
                     <i className="fas fa-upload"></i>
-                    Upload Photo
+                    {file? file.name : 'Upload Photo'}
+                    {error && (<div>{error}</div>)}
                 </label>
             </div>
         </form>
